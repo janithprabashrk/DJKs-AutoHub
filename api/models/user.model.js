52cc295eb1add7c02b25b6466a3e5ad910ffcfa9
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-09-01 15:41:55 +0530

    Create User Update API

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     },
     avatar: {
         type: String,
-        default: "https://www.google.lk/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2sykSGlA1cjfGPWVYmw-X3&ust=1724248634907000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJCZ3_Pcg4gDFQAAAAAdAAAAABAE",
+        default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
     },
 }, {timestamps : true});
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import userReducer from './user/userSlice';  // Import the reducer from the user slice
-import {persistReducer, persistStore} from 'redux-persist';  // Import the persist reducer
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const rootReducer = combineReducers({user: userReducer});  // Create a root reducer  
+const rootReducer = combineReducers({ user: userReducer });
 
 const persistConfig = {
   key: 'root',
@@ -11,14 +11,14 @@ const persistConfig = {
   version: 1,
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);  // Persist the root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,  
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-export const persistor = persistStore(store);  // Create a persistor object
+export const persistor = persistStore(store);
