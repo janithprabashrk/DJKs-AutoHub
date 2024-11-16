677a5c5672152989932d55797618feeccefde58b
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-11-16 20:15:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import CreateListing from './pages/CreateListing';
 import ShowListings from './pages/ShowListings';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
-
+import Search from './pages/Search';
 export default function App() {
   return (
     <BrowserRouter>
@@ -20,6 +20,7 @@ export default function App() {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/listing/:listingId" element={<Listing />} />
         
         <Route element={<PrivateRoute />}>
