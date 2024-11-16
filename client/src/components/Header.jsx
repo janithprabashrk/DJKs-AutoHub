eb264ce391da776584236cc363a96c779418e421
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2024-11-16 18:40:00 +0530

    Update Header.jsx
     Complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,28 @@
 import {FaSearch} from 'react-icons/fa';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate, useLocation} from 'react-router-dom';
 import {useSelector} from 'react-redux';
-
+import { useEffect, useState } from 'react';
 export default function Header() {
     const {currentUser} = useSelector(state => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const handleSearchSubmit = async (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`); 
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl);
+        }
+    }, [location.search]);
   return (
     <header className="bg-gray-800 shadow-lg">
         <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -18,8 +37,12 @@ export default function Header() {
                     type="text" 
                     placeholder="Search..."   
                     className="bg-transparent focus:outline-none w-24 sm:w-64 text-gray-200 placeholder-gray-400"
+                    value={searchTerm}
+                    onChange = {(e) => setSearchTerm(e.target.value)}
                 />
-                <FaSearch className='text-gray-400 hover:text-blue-400 transition duration-300 cursor-pointer'/>
+                <button onClick={handleSearchSubmit}>
+                    <FaSearch className='text-gray-400 hover:text-blue-400 transition duration-300 cursor-pointer'/>
+                </button>
             </form>
             <ul className='flex gap-6'>
                 <Link to='/'>
