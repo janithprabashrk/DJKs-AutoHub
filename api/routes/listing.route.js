956c3ff4d35952415285df50a134f8d71ae8c001
R.K. Janith Prabash <138957977+janithprabashrk@users.noreply.github.com>
2025-01-02 22:05:01 +0530

    Update listing.route.js

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, getListing, getListings, getUser, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, getListing, getListings, getUser, updateListing, getStats, incrementListingView, incrementAdClick } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -10,5 +10,8 @@ router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id', getListing);
 router.get('/user/:id', getUser);
 router.get('/search', getListings);
+router.get('/stats', getStats);
+router.post('/view/:id', incrementListingView);
+router.post('/click/:id', incrementAdClick);
 
 export default router;
